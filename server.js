596421986d30f06455041c1d2dd326cb4e7cb6b7
Nolan Hoffe <77253002@edu.cbe.ab.ca>
2024-10-24 13:20:45 -0600

    3 check marks

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const uuid = require("uuid")
+const uuid = require("uuid");
 const server = express();
 server.use(express.json());
 server.use(express.static('public'));
@@ -7,23 +7,36 @@ server.use(express.static('public'));
 
 //All your code goes here
 let activeSessions={}
+
 server.get('/newgame', (req, res)=>{
     let newID = uuid.v4();
     let newGame={
-        wordToGuess: ["spike"],
+        wordToGuess: "pump",
         guesses:[],
         correctLetters:[],
         closeLetters:[],
         rightLetters:[],
         remainingGuesses: 6,
         gameOver: false
+
     }
     activeSessions[newID] = newGame;
+    console.log(activeSessions);
+
     res.status(201);
-    res.send({sessionsID: newID})
+    res.send({sessionID: newID});
+    
 })
-server.get('/newstate', (req, res)=>{
+
+server.get('/gamestate', (req, res) =>{
+        res.status(200);
 })
+
+server.get('/guess', (req, res) =>{
+    console.log("good job!  You got it wrong!");
+    
+})
+
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
